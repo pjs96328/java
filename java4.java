bc4a16bf4d06156457389f361a5bb7e105e3548f
pjs96328 <pjs96328@gmail.com>
2022-06-28 19:12:56 +0900

    Add files via upload

---
+++ java4.java
@@ -0,0 +1,26 @@
+package test;
+import java.util.*;
+public class java4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [][] arr = {
+				{5,5,5,5,5},
+				{10,10,10,10,10},
+				{20,20,20,20,20},
+				{30,30,30,30,30}
+		};
+		int total = 0;
+		float average = 0;
+		for (int i=0; i<arr.length;i++) {
+			for(int j=0; j<arr[i].length; j++) {
+				total += arr[i][j];
+		}
+	}
+		average = (float)total/(arr.length);
+		System.out.println("total : " + total);
+		System.out.println("total : " + average);
+
+	}
+
+}
